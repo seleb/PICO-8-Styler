a71150bc73ccee62358139546d0af69d775806a3
seleb <sean.s.leblanc@gmail.com>
2016-09-03 00:16:56 -0400

    added gamepad support

--- template.php
+++ template.php
@@ -17,6 +17,8 @@
 
 	$includeButtons = get("includeButtons", true);
 
+	$includeGamepad = get("includeGamepad", true);
+
 	$totalHeight = $height;
 
 	if($includeButtons){
@@ -160,6 +162,46 @@
 					pico8_buttons[0] = 2 | 16; // example: player 0, RIGHT and Z held down
 					*/
 				</script>
+				<?php if($includeGamepad){ ?>
+				<script>
+					<?php echo file_get_contents("input-gamepads.js"); ?>
+				</script>
+
+				<script>
+					var pico8_buttons = [0, 0, 0, 0, 0, 0, 0, 0];
+
+					gamepads.init();
+
+					if(gamepads.available){
+						var thresh=(gamepads.deadZone+gamepads.snapZone)/2;
+						function pushGamepadToButtons(){
+							gamepads.update();
+
+							var stick=gamepads.getAxes(0,2);
+							var dpad=gamepads.getDpad();
+							var btns=[
+								stick[0] < -thresh || dpad[0] < -thresh,
+								stick[0] > thresh || dpad[0] > thresh,
+								stick[1] < -thresh || dpad[1] < -thresh,
+								stick[1] > thresh || dpad[1] > thresh,
+								gamepads.isDown(0) || gamepads.isDown(3),
+								gamepads.isDown(2) || gamepads.isDown(1)
+							];
+							
+							var input=0;
+							for(var i=0; i<btns.length;++i){
+								if(btns[i]){
+									input|=Math.pow(2,i);
+								}
+							}
+							pico8_buttons[0]=input;
+							
+							requestAnimationFrame(pushGamepadToButtons);
+						}
+						pushGamepadToButtons();
+					}
+				</script>
+				<?php } ?>
 				<script async type="text/javascript" src="<?php echo $game; ?>.js"></script>
 				<script>
 					// key blocker. prevent cursor keys from scrolling page while playing cart.
